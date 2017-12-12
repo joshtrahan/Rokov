f0b6c4463262eb8b05378b1daf5d31ed0c0d363a
Josh Trahan <josht0288@gmail.com>
2017-12-11 21:30:10 -0600

    start implementing adding data

--- com/robut/markov/SQLiteConnection.java
+++ com/robut/markov/SQLiteConnection.java
@@ -4,45 +4,45 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.SQLException;
 
 public class SQLiteConnection {
     private static final String SQLITE_PREFIX = "jdbc:sqlite:";
-    private File filepath;
     private String url;
 
     private Connection connection;
 
-    public SQLiteConnection(String filepath){
-        this.filepath = new File(filepath);
+    public SQLiteConnection(String dbPath) throws SQLException{
+        File filepath = new File(dbPath);
         try {
-            this.url = this.SQLITE_PREFIX + this.filepath.getCanonicalPath();
+            this.url = this.SQLITE_PREFIX + filepath.getCanonicalPath();
         }
         catch (IOException e){
             System.err.printf("Error connecting to file at %s: %s%n", filepath, e);
+            return;
         }
 
-        if (!this.filepath.exists()) {
+        if (!filepath.exists()) {
             createDB();
-        }
-
-        try{
             createTables();
-        }
-        catch (SQLException e){
-            System.err.printf("Error creating tables: ");
         }
     }
 
-    private void createDB(){
-        try {
-            this.connection = DriverManager.getConnection(this.url);
-            System.out.printf("Created database at: %s%n", this.url);
-        }
-        catch (SQLException e){
-            System.err.printf("Error creating database: %s%n", e.getMessage());
-        }
+    public void insertWord(int id, String word) throws SQLException{
+        PreparedStatement prepStatement = this.connection.prepareStatement("INSERT INTO WordIDs (id, word) VALUES (?, ?)");
+
+        prepStatement.setInt(1, id);
+        prepStatement.setString(2, word);
+        prepStatement.executeUpdate();
+    }
+
+    private void createDB() throws SQLException{
+        this.connection = DriverManager.getConnection(this.url);
+        System.out.printf("Created database at: %s%n", this.url);
+
+        createTables();
     }
 
     private void createTables() throws SQLException{
@@ -57,12 +57,13 @@ public class SQLiteConnection {
                 "PRIMARY KEY(preID, postID) \n" +
                 ");";
 
+
         Statement statement = this.connection.createStatement();
         statement.execute(createIdMapSql);
         statement.execute(createWordRelationSql);
     }
 
-    private void connectDB(){
+    private void connectDB() throws SQLException{
         try{
             this.connection = DriverManager.getConnection(this.url);
             System.out.printf("Connected to database at: %s%n", this.url);
